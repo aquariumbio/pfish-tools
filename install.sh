75bc9a376504bb6ae34b6d7c7be3625bc96cd310
Devin Strickland <strcklnd@uw.edu>
2021-04-27 11:12:36 -0400

    abandoned setup script

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p $HOME/bin
-wget --directory-prefix=$HOME/bin https://raw.githubusercontent.com/dvnstrcklnd/pfish-tools/main/format_test_results.py
-install -m 0755 $HOME/bin/format_test_results.py
